f7b6740cca782a45bbe7e4fcc3899e87af58d579
Amalguittoum <fa_guittoum@esi.dz>
2017-12-29 08:27:35 +0100

    formulaire

---
+++ WebApplication5/WebApplication5/Properties/AssemblyInfo.cs
@@ -0,0 +1,35 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Les informations générales relatives à un assembly dépendent de 
+// l'ensemble d'attributs suivant. Changez les valeurs de ces attributs pour modifier les informations
+// associées à un assembly.
+[assembly: AssemblyTitle("WebApplication5")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("WebApplication5")]
+[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// L'affectation de la valeur false à ComVisible rend les types invisibles dans cet assembly 
+// aux composants COM. Si vous devez accéder à un type dans cet assembly à partir de 
+// COM, affectez la valeur true à l'attribut ComVisible sur ce type.
+[assembly: ComVisible(false)]
+
+// Le GUID suivant est pour l'ID de la typelib si ce projet est exposé à COM
+[assembly: Guid("1be68c0a-4c43-4db7-92c2-906a60535dc9")]
+
+// Les informations de version pour un assembly se composent des quatre valeurs suivantes :
+//
+//      Version principale
+//      Version secondaire 
+//      Numéro de build
+//      Révision
+//
+// Vous pouvez spécifier toutes les valeurs ou indiquer les numéros de révision et de build par défaut 
+// en utilisant '*', comme indiqué ci-dessous :
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ WebApplication5/WebApplication5/assets2/js/scripts.js
@@ -0,0 +1,104 @@
+
+jQuery(document).ready(function() {
+	
+    /*
+        Fullscreen background
+    */
+    $.backstretch("assets2/img/backgrounds/1.jpg");
+    
+    /*
+        Forms show / hide
+    */
+    $('.show-register-form').on('click', function(){
+    	if( ! $(this).hasClass('active') ) {
+    		$('.show-login-form').removeClass('active');
+    		$(this).addClass('active');
+    		$('.login-form').fadeOut('fast', function(){
+    			$('.register-form').fadeIn('fast');
+    		});
+    	}
+    });
+    // ---
+    $('.show-login-form').on('click', function(){
+    	if( ! $(this).hasClass('active') ) {
+    		$('.show-register-form').removeClass('active');
+    		$(this).addClass('active');
+    		$('.register-form').fadeOut('fast', function(){
+    			$('.login-form').fadeIn('fast');
+    		});
+    	}
+    });
+    
+    /*
+        Login form validation
+    */
+    $('.l-form input[type="text"], .l-form input[type="password"], .l-form textarea').on('focus', function() {
+    	$(this).removeClass('input-error');
+    });
+    
+    $('.l-form').on('submit', function(e) {
+    	
+    	$(this).find('input[type="text"], input[type="password"], textarea').each(function(){
+    		if( $(this).val() == "" ) {
+    			e.preventDefault();
+    			$(this).addClass('input-error');
+    		}
+    		else {
+    			$(this).removeClass('input-error');
+    		}
+    	});
+    	
+    });
+    
+    /*
+        Registration form validation
+    */
+    $('.r-form input[type="text"], .r-form textarea').on('focus', function() {
+    	$(this).removeClass('input-error');
+    });
+    
+    $('.r-form').on('submit', function(e) {
+    	
+    	$(this).find('input[type="text"], textarea').each(function(){
+    		if( $(this).val() == "" ) {
+    			e.preventDefault();
+    			$(this).addClass('input-error');
+    		}
+    		else {
+    			$(this).removeClass('input-error');
+    		}
+    	});
+    	
+    });
+    
+    
+});
+function bs_input_file() {
+    $(".input-file").before(
+        function() {
+            if ( ! $(this).prev().hasClass('input-ghost') ) {
+                var element = $("<input type='file' class='input-ghost' style='visibility:hidden; height:0'>");
+                element.attr("name",$(this).attr("name"));
+                element.change(function(){
+                    element.next(element).find('input').val((element.val()).split('\\').pop());
+                });
+                $(this).find("button.btn-choose").click(function(){
+                    element.click();
+                });
+                $(this).find("button.btn-reset").click(function(){
+                    element.val(null);
+                    $(this).parents(".input-file").find('input').val('');
+                });
+                $(this).find('input').css("cursor","pointer");
+                $(this).find('input').mousedown(function() {
+                    $(this).parents('.input-file').prev().click();
+                    return false;
+                });
+                return element;
+            }
+        }
+    );
+}
+$(function() {
+    bs_input_file();
+});
